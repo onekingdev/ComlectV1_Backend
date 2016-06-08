95a2e730704adafa8d89e1d7b85bf0c43cad5513
onekingdev <onekingdev@gmail.com>
2016-06-08 08:56:25 -0500

    Shrine, reloaded

--- app/models/business.rb
+++ app/models/business.rb
@@ -4,11 +4,11 @@ class Business < ActiveRecord::Base
   # rubocop:disable Rails/HasAndBelongsToMany
   has_and_belongs_to_many :jurisdictions
 
-  mount_uploader :logo, Business::LogoUploader
+  include ImageUploader[:logo]
 
-  validates :contact_first_name, :contact_last_name, :contact_email, presence: true
-  validates :business_name, :industry, :employees, :description, presence: true
-  validates :country, :city, :state, presence: true
+  # validates :contact_first_name, :contact_last_name, :contact_email, presence: true
+  # validates :business_name, :industry, :employees, :description, presence: true
+  # validates :country, :city, :state, presence: true
 
   accepts_nested_attributes_for :user
 
