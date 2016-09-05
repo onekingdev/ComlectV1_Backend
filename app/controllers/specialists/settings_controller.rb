aa6e21a7a3f4f16407da1aebece86b57c60440c5
onekingdev <onekingdev@gmail.com>
2016-09-05 16:40:31 -0300

    Add specialist, business and project admin panels

--- app/controllers/specialists/settings_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-class Specialists::SettingsController < ApplicationController
-  before_action :require_specialist!
-
-  def show
-    @user = current_user
-  end
-end
