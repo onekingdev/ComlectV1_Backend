23fe771ce94761a12e691a3a7f69f9dad724d6c6
onekingdev <onekingdev@gmail.com>
2016-10-05 22:01:20 -0400

    Update to specialist keyword search

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: bundle exec puma -C config/puma.rb
-worker: bundle exec sidekiq -q default -q mailers -q payments -e production
+worker: bundle exec sidekiq -q default -q mailers -e production
 release: bundle exec rake db:migrate
--- app/models/charge/processing.rb
+++ app/models/charge/processing.rb
@@ -1,24 +1,28 @@
 # frozen_string_literal: true
 class Charge::Processing
-  attr_reader :charges, :project
+  attr_reader :charges, :project_id
 
   def self.process_scheduled!
-    Charge.includes(:project).scheduled.for_processing.group_by(&:project).map do |project, charges|
-      new(charges, project).process!
+    # TODO: Call from scheduled bg job
+    Charge.scheduled.where('process_after <= ?', Time.zone.now).group_by(&:project_id).map do |project_id, charges|
+      new(charges, project_id).process!
     end
   end
 
-  def initialize(charges, project)
+  def initialize(charges, project_id)
     @charges = charges
-    @project = project
+    @project_id = project_id
   end
 
   def process!
     ActiveRecord::Base.transaction do
       amount_in_cents = charges.map(&:amount_in_cents).reduce(:+)
-      business_transaction = create_business_transaction(amount_in_cents)
-      # Specialists don't get directly paid for full time roles:
-      create_specialist_payment business_transaction, amount_in_cents unless project.full_time?
+      business_transaction = Transaction::BusinessCharge.create!(
+        project_id: project_id,
+        # 15 percent service fee (BigDecimal to avoid floating point issues)
+        amount_in_cents: BigDecimal.new(amount_in_cents) * 1.15
+      )
+      create_specialist_payment business_transaction, amount_in_cents
       Charge.where(id: charges.map(&:id)).update_all(
         transaction_id: business_transaction.id,
         status: Charge.statuses[:processed]
@@ -27,16 +31,6 @@ class Charge::Processing
   end
 
   private
-
-  def create_business_transaction(amount_in_cents)
-    # Don't overcharge for full-time roles since the amount is already the fee
-    fee_multiplier = project.full_time? ? 1.0 : 1.15
-    Transaction::BusinessCharge.create!(
-      project_id: project.id,
-      # 15 percent service fee (BigDecimal to avoid floating point issues)
-      amount_in_cents: BigDecimal.new(amount_in_cents) * fee_multiplier
-    )
-  end
 
   def create_specialist_payment(parent_transaction, amount_in_cents)
     Transaction::SpecialistPayment.create!(
