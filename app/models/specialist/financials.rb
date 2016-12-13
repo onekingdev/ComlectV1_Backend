ae35d5478c35fc631e35eb258c873ee84235799a
onekingdev <onekingdev@gmail.com>
2016-12-13 13:28:12 -0600

    Stripe webhooks WIP

--- app/models/specialist/financials.rb
+++ app/models/specialist/financials.rb
@@ -26,8 +26,8 @@ class Specialist::Financials
 
   def self.processed(specialist, params)
     sort_direction = params[:sort_direction].to_s.casecmp('asc').zero? ? 'ASC' : 'DESC'
-    specialist.payments
-              .processed
+    specialist.transactions
+              .one_off
               .joins(:business)
               .order("#{PAYMENT_ORDERING[params[:sort_by] || 'date']} #{sort_direction}")
               .page(params[:page]).per(5)
