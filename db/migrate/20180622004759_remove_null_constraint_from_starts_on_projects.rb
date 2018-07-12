b13b4c66f64224ce9b1c6281fa8a0fe240a4f8ec
onekingdev <onekingdev@gmail.com>
2018-07-13 00:21:01 +0300

    Multiselect dropdown click area 100% width, interview messaging update

--- db/migrate/20180622004759_remove_null_constraint_from_starts_on_projects.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveNullConstraintFromStartsOnProjects < ActiveRecord::Migration
-  def change
-    change_column_null :projects, :starts_on, true
-  end
-end
