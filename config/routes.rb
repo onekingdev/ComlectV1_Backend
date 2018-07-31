fd33029f41bbe2073d57ff5191b9de1134d04caa
onekingdev <onekingdev@gmail.com>
2018-07-30 23:19:06 -0500

    update references to fee in activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -30,8 +30,6 @@ Rails.application.routes.draw do
   end
 
   root to: 'landing_page#show'
-  get 'turnkey' => 'turnkeys#index'
-  get 'turnkey/:page' => 'turnkeys#page'
   get 'info/:page' => 'home#page', as: :page
   get 'app_config' => 'home#app_config', format: 'js'
   get 'partnerships' => 'home#partnerships'
@@ -44,7 +42,6 @@ Rails.application.routes.draw do
     resources :ima, only: %i[index create]
   end
 
-  resources :feedback_requests, only: %i[create new]
   resources :businesses, only: %i[new create show]
   resource :business, only: %i[edit] do
     patch '/' => 'businesses#update', as: :update
