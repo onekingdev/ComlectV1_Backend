03d85247cd3560dd6e574de1bfa44a6bb21dc56e
onekingdev <onekingdev@gmail.com>
2018-08-14 16:38:31 -0600

    Update notifications.en.yml

--- app/models/project_template.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ProjectTemplate < ActiveRecord::Base
-  belongs_to :turnkey_solution
-  has_and_belongs_to_many :industries
-  has_and_belongs_to_many :jurisdictions
-end
--- db/migrate/20180809052509_add_header_text_to_turnkey_pages.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddHeaderTextToTurnkeyPages < ActiveRecord::Migration
-  def change
-    add_column :turnkey_pages, :header_text, :string
-  end
-end
