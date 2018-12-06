ae2e87f174e98cd26ee02a1843027ef1017991e8
onekingdev <onekingdev@gmail.com>
2018-12-06 12:35:45 -0800

    Ensure every /projects route is authenticated

--- app/controllers/project_messages_controller.rb
+++ app/controllers/project_messages_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class ProjectMessagesController < ApplicationController
-  prepend_before_action :authenticate_user!
-  before_action :find_project
+  before_action :authenticate_user!
 
   def index
     b_id = current_business ? current_business.id : @project.business_id
