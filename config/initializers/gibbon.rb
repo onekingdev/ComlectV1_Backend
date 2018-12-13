2357b8ce3a1bd324273c33cac056909e8d614f5f
onekingdev <onekingdev@gmail.com>
2018-12-13 12:32:30 -0500

    adds tos_agreement

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,6 +1,4 @@
-# frozen_string_literal: true
-
-Gibbon::Request.api_key = ENV.fetch('MAILCHIMP_API_KEY')
+Gibbon::Request.api_key = ENV.fetch("MAILCHIMP_API_KEY")
 Gibbon::Request.timeout = 150
 Gibbon::Request.open_timeout = 150
 # Gibbon::Request.symbolize_keys = true
