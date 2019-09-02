9bc6d67b487c230ab907b1dea490c90ee548058b
onekingdev <onekingdev@gmail.com>
2019-09-02 12:06:41 +0300

    Partnerships page

--- db/migrate/20190827194645_change_default_to_interview.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeDefaultToInterview < ActiveRecord::Migration
-  def change
-    change_column :projects, :applicant_selection, :string, default: 'interview'
-  end
-end
