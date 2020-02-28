0e4a28f308ad32b775cc89d5c83480b2f21cc27e
onekingdev <onekingdev@gmail.com>
2020-02-28 11:02:10 +0300

    fullstory update (#111)

--- app/models/audit_doc.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AuditDoc < ActiveRecord::Base
-  belongs_to :business
-  include DocUploader[:file]
-  include PdfUploader[:pdf]
-end
--- db/migrate/20191107010504_add_done_at_to_reminders.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddDoneAtToReminders < ActiveRecord::Migration
-  def change
-    add_column :reminders, :done_at, :datetime, default: nil
-  end
-end
