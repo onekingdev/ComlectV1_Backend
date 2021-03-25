f0531f42ae4af62745aae275251c23ca13c185b4
onekingdev <onekingdev@gmail.com>
2021-03-26 02:46:49 +0300

    Specialist: load LocalProject instead of Project

--- app/javascript/packs/vue/business/projects/ShowPage.vue
+++ app/javascript/packs/vue/business/projects/ShowPage.vue
@@ -19,7 +19,6 @@
                 Get(v-if="project.visible_project" :etag="etag" :project="`/api/business/projects/${project.visible_project.id}`"): template(v-slot="{project}")
                   TimesheetsNotice(:project="project")
                   EndContractNotice(:project="project" @saved="completeSuccess" @errors="completeErrors")
-                  ExtendDeadlineNotice(:project="project")
             .row.p-x-1
               .col-md-7.col-sm-12
                 .card
@@ -82,14 +81,13 @@
 
 <script>
 import { fields, readablePaymentSchedule } from '@/common/ProposalFields'
-import ApplicationsNotice from './alerts/ApplicationsNotice'
-import TimesheetsNotice from './alerts/TimesheetsNotice'
-import EndContractNotice from './alerts/EndContractNotice'
+import ApplicationsNotice from './ApplicationsNotice'
+import TimesheetsNotice from './TimesheetsNotice'
+import EndContractNotice from './EndContractNotice'
 import ProjectDetails from './ProjectDetails'
 import EtaggerMixin from '@/mixins/EtaggerMixin'
 import LocalProjectModal from './LocalProjectModal'
 import EndContractModal from './EndContractModal'
-import ExtendDeadlineNotice from './alerts/ExtendDeadlineNotice'
 
 export default {
   mixins: [EtaggerMixin],
@@ -111,7 +109,6 @@ export default {
   },
   components: {
     ApplicationsNotice,
-    ExtendDeadlineNotice,
     LocalProjectModal,
     TimesheetsNotice,
     EndContractNotice,
