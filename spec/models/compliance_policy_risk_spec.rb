4c0d61b53d9e9ca5d0ce11b20ec809cdad81082e
onekingdev <onekingdev@gmail.com>
2021-04-11 23:35:57 +0300

    Fix Discussions fetching

--- spec/models/compliance_policy_risk_spec.rb
+++ spec/models/compliance_policy_risk_spec.rb
@@ -2,6 +2,6 @@
 
 require 'rails_helper'
 
-RSpec.describe CompliancePolicyConfiguration, type: :model do
+RSpec.describe CompliancePolicyRisk, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
