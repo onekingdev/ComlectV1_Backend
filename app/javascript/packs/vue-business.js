f250ed3b3ce291cdbe3eae80ec2a8a592c6eeecc
onekingdev <onekingdev@gmail.com>
2021-05-30 19:27:21 +0300

    Default DatePicker locale

--- app/javascript/packs/vue-business.js
+++ app/javascript/packs/vue-business.js
@@ -13,13 +13,11 @@ import BusinessAnnualReviewPage from './vue/business/annual/PageCurrentReviewCat
 import BusinessAnnualGeneralPage from './vue/business/annual/PageCurrentGeneral.vue'
 
 import BusinessPoliciesPage from './vue/business/policies/Page.vue'
-import BusinessPoliciesEntirePage from './vue/business/policies/PoliciesEntire.vue'
 import BusinessPoliciesCreatePage from './vue/business/policies/Details/PolicyCreate.vue'
 import BusinessPoliciesDetailsWithoutSectionsPage from './vue/business/policies/Details/PolicyDetailsWithoutSections.vue'
 
 const businesPolicies = {
   BusinessPoliciesPage,
-  BusinessPoliciesEntirePage,
   BusinessPoliciesCreatePage,
   BusinessPoliciesDetailsWithoutSectionsPage,
 }
@@ -52,10 +50,12 @@ const BusinessFileFolders = {
 
 import BusinessExamManagementPage from './vue/business/exammanagement/Page.vue'
 import BusinessExamManagementShowPage from './vue/business/exammanagement/PageCurrentReviewExam.vue'
+import BusinessExamManagementAuditorPortalShowPage from './vue/business/exammanagement/PageAuditorPortal.vue'
 
 const BusinessExamManagement = {
   BusinessExamManagementPage,
   BusinessExamManagementShowPage,
+  BusinessExamManagementAuditorPortalShowPage,
 }
 
 
