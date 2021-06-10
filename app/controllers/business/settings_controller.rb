d92c84b72e5d3a43b7bbd895b4fbe6f441fe210d
onekingdev <onekingdev@gmail.com>
2021-06-10 19:50:03 +0600

    created notifications!

--- app/controllers/business/settings_controller.rb
+++ app/controllers/business/settings_controller.rb
@@ -1,16 +1,9 @@
 # frozen_string_literal: true
 
 class Business::SettingsController < ApplicationController
-  include ActionView::Helpers::TagHelper
-
   before_action :require_business!
 
-  # def show
-  #   @user = current_user
-  # end
-
   def show
-    render html: content_tag('business-settings-page', '').html_safe, layout: 'vue_business'
+    @user = current_user
   end
-
 end
