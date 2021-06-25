43e193ca35ebda4bd58c43ccd8d15dbd66da9700
onekingdev <onekingdev@gmail.com>
2021-06-25 21:03:59 +0600

    added access denied page

--- app/javascript/packs/vue-business.js
+++ app/javascript/packs/vue-business.js
@@ -1,5 +1,7 @@
 import init from './vue/init'
 import store from '@/store/business'
+import router from '@/router'
+import MainLayoyt from './vue/layouts/Main'
 import BusinessDashboardPage from './vue/business/dashboard/Page.vue'
 import BusinessRemindersPage from './vue/business/reminders/Page.vue'
 import BusinessPostProjectPage from './vue/business/projects/PostProjectPage.vue'
@@ -13,11 +15,13 @@ import BusinessAnnualReviewPage from './vue/business/annual/PageCurrentReviewCat
 import BusinessAnnualGeneralPage from './vue/business/annual/PageCurrentGeneral.vue'
 
 import BusinessPoliciesPage from './vue/business/policies/Page.vue'
+import BusinessPoliciesEntirePage from './vue/business/policies/PoliciesEntire.vue'
 import BusinessPoliciesCreatePage from './vue/business/policies/Details/PolicyCreate.vue'
 import BusinessPoliciesDetailsWithoutSectionsPage from './vue/business/policies/Details/PolicyDetailsWithoutSections.vue'
 
-const businesPolicies = {
+const businessPolicies = {
   BusinessPoliciesPage,
+  BusinessPoliciesEntirePage,
   BusinessPoliciesCreatePage,
   BusinessPoliciesDetailsWithoutSectionsPage,
 }
@@ -76,7 +80,9 @@ const BusinessTasks = {
 
 init({
   store,
+  router,
   components: {
+    MainLayoyt,
     BusinessDashboardPage,
     BusinessRemindersPage,
     BusinessProjectsPage,
@@ -88,7 +94,7 @@ init({
     BusinessAnnualsPage,
     BusinessAnnualReviewPage,
     BusinessAnnualGeneralPage,
-    ...businesPolicies,
+    ...businessPolicies,
     ...BusinessReportsRisks,
     ...businesRisks,
     ...businesMarketplace,
