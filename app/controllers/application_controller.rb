d726508808a9e1a1d7fe158ad253ce45f24a37d7
onekingdev <onekingdev@gmail.com>
2021-07-18 10:12:30 +0300

    added list of threads to messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,8 +29,8 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def timezones_array
-    ActiveSupport::TimeZone.all.map(&proc { |tz| [tz.tzinfo.to_s, tz.name] })
+  def timezones_json
+    ActiveSupport::TimeZone.all.map(&proc { |tz| [tz.tzinfo.to_s, tz.name] }).to_json
   end
 
   def sub_industries(specialist)
--- config/routes.rb
+++ config/routes.rb
@@ -243,7 +243,6 @@ Rails.application.routes.draw do
   end
 
   namespace :api do
-    get 'static_collection' => 'static_collection#index'
     post 'exams/:uuid' => 'exams#email'
     patch 'exams/:uuid' => 'exams#show'
 
@@ -283,6 +282,7 @@ Rails.application.routes.draw do
     get 'local_projects/:project_id/messages' => 'project_messages#index'
     post 'local_projects/:project_id/messages' => 'project_messages#create'
     resources :direct_messages, path: 'messages/:recipient_id', only: %i[index create]
+    get 'messages' => 'direct_messages#show'
     resources :project_ratings, only: %i[index]
     namespace :business do
       resources :exams, only: %i[index show create update destroy] do
