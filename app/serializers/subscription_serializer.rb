ab90b5d7a5f70655fb88b9822608900bdb3fb81a
onekingdev <onekingdev@gmail.com>
2021-07-21 00:55:13 +0300

    add test

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -7,5 +7,10 @@ class SubscriptionSerializer < ApplicationSerializer
              :updated_at,
              :kind_of,
              :title,
+             :billing_period_ends_at,
              :status
+
+  def billing_period_ends_at
+    Time.at(object.billing_period_ends).utc
+  end
 end
