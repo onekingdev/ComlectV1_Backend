1a11abe35c2fc663b50fe37ea6b0d3e61c58ed07
onekingdev <onekingdev@gmail.com>
2021-07-23 12:29:38 +0300

    fix process env

--- spec/support/controller_helper.rb
+++ spec/support/controller_helper.rb
@@ -1,18 +1,10 @@
 # frozen_string_literal: true
 
 module ControllerHelper
-  def login_as_business(user = nil)
-    user = FactoryBot.create(:business).user if user.blank?
-    login_user(user)
-  end
-
-  def login_as_specialist(user = nil)
-    user = FactoryBot.create(:specialist).user if user.blank?
-    login_user(user)
-  end
-
-  def login_user(user)
+  def login_user(user = nil)
     @request.env['devise.mapping'] = Devise.mappings[:user]
-    sign_in(user)
+    user = FactoryBot.create(:business).user if user.blank?
+
+    sign_in user
   end
 end
