0fe4d6dd006be223c8ecdd77077cb4c498f42caa
onekingdev <onekingdev@gmail.com>
2021-07-30 02:26:59 +0600

    added image for error

--- app/controllers/api/static_collection_controller.rb
+++ app/controllers/api/static_collection_controller.rb
@@ -13,7 +13,6 @@ class Api::StaticCollectionController < ApiController
                    "sub_industries_business": sub_industries(false),
                    "sub_industries_specialist": sub_industries(true),
                    "states": State.fetch_all_usa,
-                   "countries": ISO3166::Country.all.collect(&:name),
                    "timezones": timezones_array }.to_json
   end
 end
